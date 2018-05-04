7fe52e6a2453a1b9bb0265285de9001df96e7110
Hannah Kiesel <hmkiesel@gmail.com>
2018-05-04 15:34:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import facebook_james
-import starbucks
+import website_summary
 
 def main():
     print "Analysis of posts from Starbucks' Facebook"
@@ -7,10 +7,10 @@ def main():
     facebook_james.getLexicalDiversity()
     
     print("\nStarbucks.com Economic Platform: 'Q2 Fiscal 2018 Results'")
-    starbucks.get_summary("https://investor.starbucks.com/press-releases/financial-releases/press-release-details/2018/Starbucks-Reports-Record-Q2-Fiscal-2018-Results/default.aspx")
+    website_summary.get_summary("https://investor.starbucks.com/press-releases/financial-releases/press-release-details/2018/Starbucks-Reports-Record-Q2-Fiscal-2018-Results/default.aspx")
     print("\nStarbucks.com: 'about us: company information'")
-    starbucks.get_summary("https://starbucks.com/about-us/company-information")
+    website_summary.get_summary("https://starbucks.com/about-us/company-information")
     print("\nStarbucks Blog Article: 'Equal Pay for Equal Work'")
-    starbucks.get_summary("https://starbuckschannel.com/starbucks-announces-pay-equity-for-us-partners-sets-global-goal/")
+    website_summary.get_summary("https://starbuckschannel.com/starbucks-announces-pay-equity-for-us-partners-sets-global-goal/")
     
 main()
