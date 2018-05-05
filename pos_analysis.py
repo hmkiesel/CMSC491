dfdfc3faefbbd12a471dd0d7180b9854cbc4a26a
Hannah Kiesel <hmkiesel@gmail.com>
2018-05-04 23:04:09 -0400

    put all code inside function to call from main.py

--- pos_analysis.py
+++ pos_analysis.py
@@ -19,73 +19,74 @@ def removeUnicode(text):
 			asciiText = asciiText + char
 	return asciiText
 
-fileObj = codecs.open("POS.rtf","w","UTF")
-link = "https://starbucks.com/about-us/company-information"
-html = requests.get(link)
+def get_pos():
+	fileObj = codecs.open("POS.rtf","w","UTF")
+	link = "https://starbucks.com/about-us/company-information"
+	html = requests.get(link)
 
-soup = BeautifulSoup(html.text,'html5lib')
-all_paras = soup.find_all('p')
+	soup = BeautifulSoup(html.text,'html5lib')
+	all_paras = soup.find_all('p')
 
-data_2017 = ""
-for para in all_paras:
-	fileObj.write(para.text)
-	data_2017 = data_2017 + para.text
-asc_2017 = removeUnicode(data_2017)
+	data_2017 = ""
+	for para in all_paras:
+		fileObj.write(para.text)
+		data_2017 = data_2017 + para.text
+	asc_2017 = removeUnicode(data_2017)
 
-lstSent = nltk.tokenize.sent_tokenize(asc_2017)
-	
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	
+	lstSent = nltk.tokenize.sent_tokenize(asc_2017)
 
-print "Part Of Speech Tags for " + link + " :"
+	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	
 
-sentWords = [nltk.tokenize.word_tokenize(s) for s in lstSent]
-posWords = [nltk.pos_tag(w) for w in sentWords]
-posWords = [token for sent in posWords for token in sent]
+	print "Part Of Speech Tags for " + link + " :"
 
-#THIS IS THE ORIGINAL PRINTING LOOP - It simply prints the word and the part of speech
-#for (token, pos) in posWords:
-#	print token, pos
+	sentWords = [nltk.tokenize.word_tokenize(s) for s in lstSent]
+	posWords = [nltk.pos_tag(w) for w in sentWords]
+	posWords = [token for sent in posWords for token in sent]
 
-#GET ALL TAGS
-partsofspeech = []
-for (token, pos) in posWords:
-	if pos not in partsofspeech:
-		partsofspeech.append(pos)
-	#print token, pos
-print partsofspeech
-print "Total # of tags: ",
-print len(partsofspeech)
+	#THIS IS THE ORIGINAL PRINTING LOOP - It simply prints the word and the part of speech
+	#for (token, pos) in posWords:
+	#	print token, pos
 
-#PRINT SPECIFIC PARTS OF SPEECH
-for i in partsofspeech:
-	count = 0
-	print "\n[ Part Of Speech tag: " + i + " ]"
+	#GET ALL TAGS
+	partsofspeech = []
 	for (token, pos) in posWords:
-		if i == pos:
-			print token,
-			count += 1
-	print "\n=> [ " + i + " ] count is:", 
-	print count
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	
+		if pos not in partsofspeech:
+			partsofspeech.append(pos)
+		#print token, pos
+	print partsofspeech
+	print "Total # of tags: ",
+	print len(partsofspeech)
 
-chunkCollector = []
-foundChunk = []
+	#PRINT SPECIFIC PARTS OF SPEECH
+	for i in partsofspeech:
+		count = 0
+		print "\n[ Part Of Speech tag: " + i + " ]"
+		for (token, pos) in posWords:
+			if i == pos:
+				print token,
+				count += 1
+		print "\n=> [ " + i + " ] count is:", 
+		print count
+	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	
 
-lastPos = None
-for(token, pos) in posWords:
-	if pos == lastPos and pos.startswith('NN'):
-		foundChunk.append(token)
-	elif pos.startswith('NN'):
-		if foundChunk != []:
-			chunkCollector.append((''.join(foundChunk),pos))
-		foundChunk = [token]
-	lastPos = pos
+	chunkCollector = []
+	foundChunk = []
 
-dChunk = {}
-for chunk in chunkCollector:
-	dChunk[chunk] = dChunk.get(chunk, 0) + 1
+	lastPos = None
+	for(token, pos) in posWords:
+		if pos == lastPos and pos.startswith('NN'):
+			foundChunk.append(token)
+		elif pos.startswith('NN'):
+			if foundChunk != []:
+				chunkCollector.append((''.join(foundChunk),pos))
+			foundChunk = [token]
+		lastPos = pos
 
-print "\nCount Frequency for each Chunk using Russell's Algorithm:"
-for (entity, pos) in dChunk:
-	if entity.istitle():
-		print '\t%s (%s)' % (entity, dChunk[entity, pos])
+	dChunk = {}
+	for chunk in chunkCollector:
+		dChunk[chunk] = dChunk.get(chunk, 0) + 1
+
+	print "\nCount Frequency for each Chunk using Russell's Algorithm:"
+	for (entity, pos) in dChunk:
+		if entity.istitle():
+			print '\t%s (%s)' % (entity, dChunk[entity, pos])
