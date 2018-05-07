b6e23bda13e990ddfe4084006c8bf577e568793c
Hemang Bhatt <hemang.bhatt@outlook.com>
2018-05-07 02:46:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import get_tweets
+import twitter_hemang
 import bigrams
 import facebook_james
 import website_summary
 import pos_analysis
 
 def main():
-    print "Analysis of tweets from Starbucks"
-    get_tweets()
+    auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,CONSUMER_KEY,CONSUMER_SECRET)
+    tw = twitter.Twitter(auth=auth)
+    print "Analysis of tweets from Starbucks\n"
+    get_tweets(tw)
     print "*" * 30 + 'END' + "*" * 30 
     
     print "Analysis of posts from Starbucks' Facebook"
     facebook_james.printPosts()
     facebook_james.getLexicalDiversity()
+    print "*" * 30 + 'END' + "*" * 30
     
     print "\nStarbucks.com Economic Platform: 'Q2 Fiscal 2018 Results'"
     website_summary.get_summary("https://investor.starbucks.com/press-releases/financial-releases/press-release-details/2018/Starbucks-Reports-Record-Q2-Fiscal-2018-Results/default.aspx")
@@ -20,13 +23,14 @@ def main():
     print "\nStarbucks Blog Article: 'Equal Pay for Equal Work'"
     website_summary.get_summary("https://starbuckschannel.com/starbucks-announces-pay-equity-for-us-partners-sets-global-goal/")
     website_summary.scrape_profile()
-    
+    print "*" * 30 + 'END' + "*" * 30
     
     print "\nPOS Analysis of Starbucks Website 'about-us' page\n"
     pos_analysis.get_pos()
-    
+    print "*" * 30 + 'END' + "*" * 30
     
     print "\nFind Bigrams on Starbucks Website 'about-us' page\n"
     bigrams.findBigrams()
+    print "*" * 30 + 'END' + "*" * 30
     
 main()
